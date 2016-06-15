8f918d25e572124040d2d314a853a89c991a6c63
Jayamine Alupotha <jayamine.alupotha@gmail.com>
2016-06-16 00:58:31 +0530

    added a new example
    added a simple example and change file structure

---
+++ pythonClient/example/Simple Example/client/Client.py
@@ -0,0 +1,45 @@
+import Eventbus.Eventbus as Eventbus
+import Eventbus.DeliveryOption as DeliveryOption
+import json
+
+#replyHanlder (self,error,message)
+#handlers (self,message)
+
+class Client:
+
+	#replyHandler 
+	def replyhandler(self,error,message):
+		if error != None:
+			print(error)
+		if message != None:
+			print(message['body'])
+	
+	#Handler for errors and msg
+	def Handler(self,message):
+		if message != None:
+			print(message['body'])
+			
+	
+eb=Eventbus.Eventbus('localhost', 7000)	
+
+#jsonObject -body
+body={'msg':'hi server!',}
+
+#DeliveryOption
+do=DeliveryOption.DeliveryOption();
+do.addHeader('type','text')
+do.addHeader('size','small')
+do.addReplyAddress('welcome')
+do.setTimeInterval(1) 
+
+#register handler
+eb.registerHandler('welcome',do,Client.Handler);
+
+#send 
+eb.send('welcome',body,do)
+
+#send
+eb.send('ignore',body,do,Client.replyhandler)
+
+#close after 5 seconds
+eb.closeConnection(5)

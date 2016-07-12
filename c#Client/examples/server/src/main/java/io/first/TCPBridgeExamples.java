57e02e8e4af4bc11850f58681202e731fba5bc2d
Jayamine Alupotha <jayamine.alupotha@gmail.com>
2016-07-13 01:11:24 +0530

    added : client

--- c#Client/examples/server/src/main/java/io/first/TCPBridgeExamples.java
+++ c#Client/examples/server/src/main/java/io/first/TCPBridgeExamples.java
@@ -15,20 +15,18 @@ import io.vertx.core.eventbus.EventBus;
 
 /**
  *
- * @author <a href="mailto:plopes@redhat.com">Paulo Lopes</a>
+ * @author Jayamine Alupotha
  */
 public class TCPBridgeExamples extends AbstractVerticle{
 
   public void start(Future<Void> fut){
-	System.out.println("\nstep1\n");
+
 	TcpEventBusBridge bridge = TcpEventBusBridge.create(
     vertx,
     new BridgeOptions()
         .addInboundPermitted(new PermittedOptions().setAddress("pcs.status"))
         .addOutboundPermitted(new PermittedOptions().setAddress("pcs.status"))
-		.addInboundPermitted(new PermittedOptions().setAddress("pcs.status.c"))
-		.addOutboundPermitted(new PermittedOptions().setAddress("pcs.status.c")));
-	System.out.println("\nstep2\n");
+		
 	bridge.listen(7000, res -> {
 		System.out.println("Started");
 		if (res.succeeded()) {
@@ -37,9 +35,9 @@ public class TCPBridgeExamples extends AbstractVerticle{
 		System.out.println("request failed");
 		}
 	});
-	System.out.println("\nstep3\n");
+	
 	EventBus eb = vertx.eventBus();
-	System.out.println("\nstep4\n");
+	
 	eb.consumer("pcs.status", message -> {
 		System.out.println("I have received a message for pcs.status: " + message.body());
 		System.out.println("headers:"+message.headers());
@@ -48,34 +46,5 @@ public class TCPBridgeExamples extends AbstractVerticle{
 		message.reply(object);
 	});
 	
-	
-	
-	//consumer is a function in eventbus
-    /*MessageConsumer<JsonObject> consumer = eb.consumer("pcs.status");
-	consumer.handler(message -> {
-		System.out.println("I have received a message");
-		System.out.println("headers: "+message.headers().toString());
-		System.out.println("Body   : "+message.body());
-		message.reply("i got it");
-		System.out.println("ok");
-	});
-	
-	MessageConsumer<JsonObject> consumer1 = eb.consumer("pcs.status.permit");
-	consumer1.handler(message -> {
-		System.out.println("I have received a message for permit");
-		System.out.println("headers: "+message.headers().toString());
-		System.out.println("Body   : "+message.body());
-		message.reply("i got it");
-		System.out.println("ok");
-	});
-	
-	
-	consumer.completionHandler(res -> {
-	if (res.succeeded()) {
-		System.out.println("The handler registration has reached all nodes");
-	} else {
-		System.out.println("Registration failed!");
-	}
-	});*/
   }
 }

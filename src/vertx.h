e94b8470cd2d137941a28774089c3358dcaf598f
Jayamine Alupotha <jayamine.alupotha@gmail.com>
2016-07-28 22:37:37 +0530

    Update vertx.h

--- src/vertx.h
+++ src/vertx.h
@@ -14,5 +14,5 @@ void eventbus_send(String address,String replyAddress,String Headers,String Body
 void eventbus_publish(String address,String Headers,String Body);
 void eventbus_register(String address,String Headers,String Body,void (*func)(String *));
 void eventbus_unregister(String address,String Headers,String Body);
-void test(void (*func)(String *));
+
 #endif

7121d0265de1eb81ce7454f5dd665408eaae1173
Julien Viet <julien@julienviet.com>
2017-06-04 18:56:18 +0200

    Add support for headers

--- java/src/main/java/io/vertx/ext/eventbus/client/Message.java
+++ java/src/main/java/io/vertx/ext/eventbus/client/Message.java
@@ -1,20 +1,28 @@
 package io.vertx.ext.eventbus.client;
 
+import java.util.Map;
+
 /**
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
  */
 public class Message<T> {
 
   private final String address;
+  private final Map<String, String> headers;
   private final T body;
 
-  public Message(String address, T body) {
+  public Message(String address, Map<String, String> headers, T body) {
     this.address = address;
+    this.headers = headers;
     this.body = body;
   }
 
   public String address() {
     return address;
+  }
+
+  public Map<String, String> headers() {
+    return headers;
   }
 
   public T body() {

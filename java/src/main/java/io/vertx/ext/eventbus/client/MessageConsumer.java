7514fe6df45d4368c044fb792ac4d895e60d1b9f
Lin Gao <aoingl@gmail.com>
2021-01-05 15:55:39 +0800

    Added Javadoc for public APIs

--- java/src/main/java/io/vertx/ext/eventbus/client/MessageConsumer.java
+++ java/src/main/java/io/vertx/ext/eventbus/client/MessageConsumer.java
@@ -3,6 +3,13 @@ package io.vertx.ext.eventbus.client;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
+ * An event bus message consumer represents a registered {@link Handler} on the specified {@code address}.
+ * <p>
+ * It is possible to have multiple {@code MessageConsumer} instances on the same address, the handlers will be called
+ * when a message is received from event bus, the order of the handlers to be invoked is the register order.
+ * <p>
+ * The consumer is unregistered from the event bus using the {@link #unregister()} method.
+ *
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
  */
 public class MessageConsumer<T> {
@@ -12,6 +19,13 @@ public class MessageConsumer<T> {
   final MessageHandler<T> handler;
   private final AtomicBoolean registered = new AtomicBoolean(true);
 
+  /**
+   * Constructor of the MessageConsumer.
+   *
+   * @param client the {@link EventBusClient} used to unregister the handler from event bus.
+   * @param address the address to monitor the messages from event bus.
+   * @param handler the handler which will be called when a message is received from event bus.
+   */
   MessageConsumer(final EventBusClient client, final String address, final Handler<Message<T>> handler) {
     this.client = client;
     this.address = address;
@@ -28,6 +42,11 @@ public class MessageConsumer<T> {
     };
   }
 
+  /**
+   * Unregister the {@code handler} from the event bus.
+   * <p>
+   * The handler is only unregistered once, multiple invokes to this method do nothing.
+   */
   public void unregister() {
     if (this.registered.compareAndSet(true, false)) {
       this.client.unregister(this.handler, true);

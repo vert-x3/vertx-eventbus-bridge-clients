3d90839195edc054f856f36627f8d2cea07e648f
Lin Gao <aoingl@gmail.com>
2021-02-07 15:49:22 +0800

    Enable github actions CI for python client

--- python/setup.py
+++ python/setup.py
@@ -1,9 +1,20 @@
-from distutils.core import setup
+from os import path
+import setuptools
 
-setup(
-    name='Vertx',
-    version='3.4.0',
-    packages=['Vertx',],
+pwd = path.abspath(path.dirname(__file__))
+rdm_file = path.join(pwd, 'README.md')
+with open(rdm_file, encoding='utf-8') as f:
+    readme = f.read()
+
+setuptools.setup(
+    name='vertx-eventbus-client',
+    version='1.0.0.dev0',
+    packages=['Vertx'],
+    author='vertx-dev',
+    author_email='vertx-dev@googlegroups.com',
+    url='https://github.com/vert-x3/vertx-eventbus-bridge-clients/tree/master/python',
     license='Apache Software License 2.0',
-    long_description=open('README').read(),
+    description='Vertx EventBus Client',
+    long_description=readme,
+    long_description_content_type='text/markdown'
 )

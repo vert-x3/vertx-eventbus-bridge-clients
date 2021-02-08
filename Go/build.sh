a05ae6c81d764fe1aa4bca69dfcf40bfb394ae9d
Lin Gao <aoingl@gmail.com>
2021-02-08 13:23:33 +0800

    Enable github actions CI for Go client

--- Go/build.sh
+++ Go/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -e
-mkdir -p /go/src/github.com/vert-x3/vertx-eventbus-bridge-clients/Go
-cd /go/src/github.com/vert-x3/vertx-eventbus-bridge-clients/Go
-ln -s /workdir/eventbus
+cwd=$(cd `dirname $0` && pwd)
+wd="${GOPATH}/src/github.com/vert-x3/vertx-eventbus-bridge-clients/Go"
+mkdir -p $wd
+cd $wd
+[ ! -s eventbus ] && ln -s $cwd/eventbus
 cd eventbus
 go get
 go build -v
-cd /workdir/sample-vertx-go-client
+cd $cwd/sample-vertx-go-client
 go build -v

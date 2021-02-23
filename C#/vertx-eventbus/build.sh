92f3f2c8205fd09efd4c2d5602838371db1739f1
Lin Gao <aoingl@gmail.com>
2021-02-23 14:45:57 +0800

    Do not run release on new tags

--- C#/vertx-eventbus/build.sh
+++ C#/vertx-eventbus/build.sh
@@ -1,5 +1,42 @@
 #!/bin/sh
 set -e
-cd /workdir/vertx-eventbus
+cwd=$(cd `dirname $0` && pwd)
+cd $cwd
+
+dist=0
+publish=0
+apikey=""
+
+while (( "$#" )); do
+  case "$1" in
+  "--dist"*)
+    dist=1
+    shift
+    ;;
+  "--publish"*)
+    publish=1
+    shift
+    ;;
+  "--apikey"*)
+    apikey="$2"
+    shift
+    shift
+    ;;
+  *)
+    ;;
+  esac
+done
+
+echo -e "Build the project"
 dotnet restore
-dotnet build
+dotnet build --no-restore -c Release
+
+if [ $dist -eq 1 -o $publish -eq 1 ]; then
+  echo -e "Package the project"
+  dotnet pack --include-symbols -p:SymbolPackageFormat=snupkg --no-build -c Release vertx-eventbus.csproj -o .
+fi
+
+if [ $publish -eq 1 ]; then
+  echo -e "Publish the package to Nuget"
+  dotnet nuget push *.nupkg -s https://api.nuget.org/v3/index.json -k $apikey --interactive --skip-duplicate
+fi
